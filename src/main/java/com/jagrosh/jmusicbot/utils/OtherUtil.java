2f76f438c15fa5c1b59faf69d0c44012e4085d0a
Justin denBroeder <justin.denbroeder@gmail.com>
2024-01-26 03:13:30 -0800

    Fix out of date version message

--- src/main/java/com/jagrosh/jmusicbot/utils/OtherUtil.java
+++ src/main/java/com/jagrosh/jmusicbot/utils/OtherUtil.java
@@ -45,12 +45,12 @@ public class OtherUtil
                     + "New Version: %s\n\n"
                     + "Please visit https://github.com/jagrosh/MusicBot/releases/latest to get the latest release.";
     private final static String WINDOWS_INVALID_PATH = "c:\\windows\\system32\\";
-    
+
     /**
      * gets a Path from a String
      * also fixes the windows tendency to try to start in system32
      * any time the bot tries to access this path, it will instead start in the location of the jar file
-     * 
+     *
      * @param path the string path
      * @return the Path object
      */
@@ -68,10 +68,10 @@ public class OtherUtil
         }
         return result;
     }
-    
+
     /**
      * Loads a resource from the jar as a string
-     * 
+     *
      * @param clazz class base object
      * @param name name of resource
      * @return string containing the contents of the resource
@@ -89,10 +89,10 @@ public class OtherUtil
             return null;
         }
     }
-    
+
     /**
      * Loads image data from a URL
-     * 
+     *
      * @param url url of image
      * @return inputstream of url
      */
@@ -100,7 +100,7 @@ public class OtherUtil
     {
         if(url==null)
             return null;
-        try 
+        try
         {
             URL u = new URL(url);
             URLConnection urlConnection = u.openConnection();
@@ -110,10 +110,10 @@ public class OtherUtil
         catch(IOException | IllegalArgumentException ignore) {}
         return null;
     }
-    
+
     /**
      * Parses an activity from a string
-     * 
+     *
      * @param game the game, including the action such as 'playing' or 'watching'
      * @return the parsed activity
      */
@@ -140,12 +140,12 @@ public class OtherUtil
         }
         return Activity.playing(game);
     }
-   
+
     public static String makeNonEmpty(String str)
     {
         return str == null || str.isEmpty() ? "\u200B" : str;
     }
-    
+
     public static OnlineStatus parseStatus(String status)
     {
         if(status==null || status.trim().isEmpty())
@@ -153,28 +153,28 @@ public class OtherUtil
         OnlineStatus st = OnlineStatus.fromKey(status);
         return st == null ? OnlineStatus.ONLINE : st;
     }
-    
+
     public static void checkJavaVersion(Prompt prompt)
     {
         if(!System.getProperty("java.vm.name").contains("64"))
-            prompt.alert(Prompt.Level.WARNING, "Java Version", 
+            prompt.alert(Prompt.Level.WARNING, "Java Version",
                     "It appears that you may not be using a supported Java version. Please use 64-bit java.");
     }
-    
+
     public static void checkVersion(Prompt prompt)
     {
         // Get current version number
         String version = getCurrentVersion();
-        
+
         // Check for new version
         String latestVersion = getLatestVersion();
-        
+
         if(latestVersion!=null && !latestVersion.equals(version))
         {
             prompt.alert(Prompt.Level.WARNING, "JMusicBot Version", String.format(NEW_VERSION_AVAILABLE, version, latestVersion));
         }
     }
-    
+
     public static String getCurrentVersion()
     {
         if(JMusicBot.class.getPackage()!=null && JMusicBot.class.getPackage().getImplementationVersion()!=null)
@@ -182,13 +182,13 @@ public class OtherUtil
         else
             return "UNKNOWN";
     }
-    
+
     public static String getLatestVersion()
     {
         try
         {
             Response response = new OkHttpClient.Builder().build()
-                    .newCall(new Request.Builder().get().url("https://api.github.com/repos/jagrosh/MusicBot/releases/latest").build())
+                    .newCall(new Request.Builder().get().url("https://api.github.com/repos/jdenbroeder/MusicBot/releases/latest").build())
                     .execute();
             ResponseBody body = response.body();
             if(body != null)
